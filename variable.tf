f7f12e54aa7b2fa07aeff87c3008b07b588961c5
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2021-11-26 15:28:04 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,15 @@
 variable "region" {
+    default = "ap-south-1"
     type = string
     description = "The AWS region"
 }
 variable "instance_type" {
+    default = "t2.micro"
     type = string
     description = "The AWS Instane type"
 }
 variable "key_name" {
+    default = "terraformkeypair"
     type = string
     description = "The AWS instance key name"
 }
