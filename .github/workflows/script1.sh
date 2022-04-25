c88c07f325e775a26eb408c5832f4278ab957467
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2022-04-25 19:19:18 +0530

    Update script1.sh

--- .github/workflows/script1.sh
+++ .github/workflows/script1.sh
@@ -1,23 +1,4 @@
-name: Close Pull Request
-
-# only trigger on pull request closed events
-on:
-  pull_request:
-    types: [ closed ]
-
-jobs:
-  merge_job:
-    # this job will only run if the PR has been merged
-    if: github.event.pull_request.merged == true
-    runs-on: ubuntu-latest
-    steps:
-    - run: |
-        echo PR #${{ github.event.number }} has been merged
-
-  close_job:
-    # this job will only run if the PR has been closed without being merged
-    if: github.event.pull_request.merged == false
-    runs-on: ubuntu-latest
-    steps:
-    - run: |
-        echo PR #${{ github.event.number }} has been closed without being merged
+var1=10
+var2=30
+echo $var1
+echo $var2
