82c3b93cf99bca9b534ca2714080a2f691b3c66d
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2022-04-25 17:14:56 +0530

    Update script1.sh

--- .github/workflows/script1.sh
+++ .github/workflows/script1.sh
@@ -6,10 +6,4 @@ if [ $var2 -gt $var1 ]
     then
         echo "$var2 is greater than $var1"
 fi
-# Second comparison
-If [ $var1 -gt 30]
-    then
-        echo "$var is greater than 30"
-    else
-        echo "$var1 is less than 30"
-fi
+
