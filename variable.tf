3b6eae1d797674ecb39389b1f407c8a344b4c3e3
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2022-04-26 12:37:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "region" {
     default = "ap-south-1"
     type = string
-    description = "The AWS region for the test VM is ap-south-1"
+    description = "The AWS region for the test VM is ap-south-1a"
 }
 variable "instance_type" {
     default = "t2.micro"
