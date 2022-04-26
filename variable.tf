42f847680730dd3fbbace68afcf6b01aaf6805cd
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2022-04-26 13:49:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "instance_type" {
     default = "t2.micro"
     type = string
-    description = "The AWS Instane type for the VM"
+    description = "The AWS Instane type for the VM in ap-south-1a"
 }
 variable "key_name" {
     default = "terraformkeypair"
